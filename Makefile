e878722c4be564bdfc15ecdaaa8afe64507d5590
thaddeus002 <thaddeus.dupont@free.fr>
2018-02-18 23:05:50 +0100

    First Makefile

---
+++ Makefile
@@ -0,0 +1,13 @@
+
+CC=gcc
+PROG=drawbar
+
+all: $(PROG)
+
+
+yImage/libyImage.a:
+	make -C yImage libyImage.a
+
+
+drawbar: drawbar.c yImage/libyImage.a
+	$(CC) -o drawbar $< -LyImage -lyImage
---
+++ drawbar.c
@@ -0,0 +1,26 @@
+/*
+ * This program is release under GPL license.
+ * \author Yannick Garcia
+ * \since 2018
+ */
+
+/**
+ * \file drawbar.c
+ * \brief main program file for drawbar.
+ *
+ * Drawbar is a command line program to create an image of a progressbar.
+ */
+
+
+
+
+int main(int argc, char **argv) {
+
+
+
+
+
+
+    return 0;
+}
+
