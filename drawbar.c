9c0127ce2561d25b3964df7dceaf8dbc6b84691d
thaddeus002 <thaddeus.dupont@free.fr>
2018-02-20 08:17:39 +0100

    Write values aside the progressbar

--- drawbar.c
+++ drawbar.c
@@ -1,7 +1,8 @@
 /*
- * This program is release under GPL license.
- * \author Yannick Garcia
- * \since 2018
+ * Copyright (c) 2018 Yannick Garcia <thaddeus.dupont@free.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GPL license. See file LICENSE for details.
  */
 
 /**
@@ -16,6 +17,7 @@
 #include <stdlib.h>
 #include "yImage.h"
 #include "yImage_io.h"
+#include "yText.h"
 
 // input images
 #define BACK   "share/empty.png"
@@ -56,17 +58,18 @@ void progressTo(int index, yImage *bar, yImage *first, yImage *normal, yImage *l
         progress = last;
     }
 
-    y_superpose_images(bar, progress, X_OFFSET + index, Y_OFFSET);
+    if(progress != NULL) {
+        y_superpose_images(bar, progress, X_OFFSET + index, Y_OFFSET);
+    }
 }
 
 
 /**
  * Create the output image.
  * \param rate the progression to show
- * \param output filename of the file to create
- * \return 0 in case of success
+ * \return The created image or NULL
  */
-int create_bar(float rate, char *output) {
+yImage *create_bar(float rate) {
 
     yImage *bar, *first, *normal, *last;
     int err;
@@ -76,25 +79,22 @@ int create_bar(float rate, char *output) {
     bar = y_load_png(BACK);
     if(bar == NULL) {
         fprintf(stderr, "Could not read %s\n", BACK);
-        return 1;
+        return NULL;
     }
 
     first = y_load_png(FIRST);
     if(first == NULL) {
         fprintf(stderr, "Could not read %s\n", FIRST);
-        return 1;
     }
 
     normal = y_load_png(NORMAL);
     if(normal == NULL) {
         fprintf(stderr, "Could not read %s\n", NORMAL);
-        return 1;
     }
 
     last = y_load_png(LAST);
     if(last == NULL) {
         fprintf(stderr, "Could not read %s\n", LAST);
-        return 1;
     }
 
     position = (int) (rate * BAR_LENGHT);
@@ -102,10 +102,95 @@ int create_bar(float rate, char *output) {
         progressTo(i, bar, first, normal, last);
     }
 
-    err = y_save_png(bar, output);
-    y_destroy_image(bar);
-    return err;
+    return bar;
 }
+
+
+/**
+ * \return the number of characters needed to write the given number
+ */
+int numberChar(int value) {
+    int i = 0;
+
+    if(value==0) return 1;
+
+    while(value>0) {
+        i++;
+        value = value / 10;
+    }
+
+    return i;
+}
+
+
+/**
+ * Create the text to illustrate the progressbar.
+ */
+yImage *createText(int n, int t, int height) {
+
+    yImage *textImage;
+    int nb = 0; // the number of caracters to write
+    int err;
+    char text[100];
+
+    nb = numberChar(n) + numberChar(t) + 3;
+
+    textImage = y_create_image(&err, NULL, nb*9+18, height);
+    if(err) return NULL;
+
+    y_transp(textImage);
+    sprintf(text, "  %d/%d", n, t);
+    y_display_text(textImage, 0, (height-12) / 2, text);
+
+    return textImage;
+}
+
+
+
+yImage *concat(yImage *bar, yImage *text){
+
+    int l, h;
+    yImage *image;
+    int err;
+    int x = 0;
+
+    l = 0;
+    h = 0;
+
+    if(bar != NULL) {
+        h = bar->rgbHeight;
+        l += bar->rgbWidth;
+    }
+
+    if(text != NULL) {
+        if(text->rgbHeight > h) {
+            h = text->rgbHeight;
+        }
+        l += text->rgbWidth;
+    }
+
+    if(l==0 || h==0) {
+        return NULL;
+    }
+
+    image = y_create_image(&err, NULL, l, h);
+    if(err) return NULL;
+
+    y_transp(image);
+
+    if(bar != NULL) {
+        y_superpose_images(image, bar, x, 0);
+        x+=bar->rgbWidth;
+    }
+
+    if(text != NULL) {
+        y_superpose_images(image, text, x, 0);
+    }
+
+    return image;
+}
+
+
 
 
 /**
@@ -116,6 +201,11 @@ int main(int argc, char **argv) {
 
     int n, t;
     float rate; // n/t
+    yImage *bar;
+    yImage *textImage;
+    int height = 20;
+    yImage *progressBar;
+    int err = -1;
 
     if(argc < 3) {
         usage(argv[0]);
@@ -125,6 +215,21 @@ int main(int argc, char **argv) {
     t=atoi(argv[2]);
     rate=(float) n/t;
 
-    return create_bar(rate, OUTPUT);
-}
+    bar = create_bar(rate);
+    if(bar!=NULL) {
+        height = bar->rgbHeight;
+    }
 
+    textImage = createText(n, t, height);
+    progressBar = concat(bar, textImage);
+
+    y_destroy_image(bar);
+    y_destroy_image(textImage);
+
+    if(progressBar != NULL) {
+        err = y_save_png(progressBar, OUTPUT);
+        y_destroy_image(progressBar);
+    }
+
+    return err;
+}
